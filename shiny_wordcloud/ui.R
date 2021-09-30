31707ca21e5a6c078ceac7515cb4ecec33d5df87
VictorEnamorado <69372988+VictorEnamorado@users.noreply.github.com>
2021-09-30 15:15:41 -0600

    up

---
+++ shiny_wordcloud/ui.R
@@ -0,0 +1,82 @@
+ 
+
+library(shiny)
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
+
+
+##################################################################################################################
+
+text <- read.delim2("C:/Users/enamo/Desktop/Shiny-R/ds_wordcloud/buko.txt", encoding="UTF-8")
+
+# Load the data as a corpus
+docs <- Corpus(VectorSource(text))
+inspect(docs)
+
+toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+docs <- tm_map(docs, toSpace, "/")
+docs <- tm_map(docs, toSpace, "@")
+docs <- tm_map(docs, toSpace, "\\|")
+
+# Convert the text to lower case
+docs <- tm_map(docs, content_transformer(tolower))
+# Remove numbers
+docs <- tm_map(docs, removeNumbers)
+# Remove english common stopwords
+docs <- tm_map(docs, removeWords, stopwords("english"))
+# Remove your own stop word
+# specify your stopwords as a character vector
+docs <- tm_map(docs, removeWords, c("blabla1", "blabla2")) 
+# Remove punctuations
+docs <- tm_map(docs, removePunctuation)
+# Eliminate extra white spaces
+docs <- tm_map(docs, stripWhitespace)
+
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+
+
+
+##################################################################################################################
+
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            plotOutput("distPlot", height = "500px" ),
+            
+            
+            
+            
+            fluidRow(
+                column(6, align="center", DTOutput("repetidos")),
+                column(6, align="center", DTOutput("frecuencia"), 
+                       
+                ), 
+            
+            
+             
+            
+        )
+        )
+    )
+))
