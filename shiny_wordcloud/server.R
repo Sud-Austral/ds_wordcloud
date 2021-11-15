2410c08531389e564a0376af703b17413bf02741
christian <ccastro@gmx.us>
2021-11-15 18:27:33 -0300

    up

--- shiny_wordcloud/server.R
+++ shiny_wordcloud/server.R
@@ -1,40 +1,174 @@
- 
+
 library(shiny)
+library(dplyr)
 library("tm")
 library("SnowballC")
 library("wordcloud")
 library("RColorBrewer")
 library(DT)
+library(wordcloud2)
+library(memoise)
+library(plotly)
+library(shinyjs)
+##################################################################################################################
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        set.seed(1234)
-        wordcloud(words = d$word, freq = d$freq, min.freq = 1,
-                  max.words=200, random.order=FALSE, rot.per=0.35, 
-                  colors=brewer.pal(8, "Dark2"))
-
+shinyServer(function(input, output, session) {
+  
+  
+  
+  observeEvent(input$go, {
+    toggle("div_tabla1")
+  })
+  
+  observeEvent(input$go, {
+    toggle("div_plot1")
+  })
+  
+  
+  
+  fn_parrafo = reactive({
+    
+    if (length(input$parrafo) == 0) {
+      
+    } else {       
+      link_1 <- import("https://raw.githubusercontent.com/VictorEnamorado/prueba_nube/main/buko2.xlsx")
+      req(input$parrafo)
+      direccion <- link_1[as.numeric(input$parrafo),2] 
+      options(encoding="utf-8")
+      text <- read.delim2(direccion)
+      
+      texto <- paste0()
+      for (i in 1:length(text)) {
+        texto <- paste0(texto,text[i])
+      }
+      
+      docs <- Corpus(VectorSource(texto))
+      toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+      docs <- tm_map(docs, toSpace, "/")
+      docs <- tm_map(docs, toSpace, "@")
+      docs <- tm_map(docs, toSpace, "\\|") 
+      docs <- tm_map(docs, content_transformer(tolower)) 
+      docs <- tm_map(docs, removeNumbers) 
+      docs <- tm_map(docs, removeWords, stopwords("english"))  
+      # Artículos
+      docs <- tm_map(docs, removeWords, c("el","la","los","las","un","una","unos","unas","del" ))
+      # Conjunciones
+      docs <- tm_map(docs, removeWords, c("y", "e", "ni", "que", "pero", "mas", "aunque", "sino", "siquiera", "o", "u", "ora", "sea",
+                                          "bien", "ya", "cerca","lejos", "éste","aquél", "pues", "porque", "puesto", "ya que",
+                                          "como", "más","que", "igual que", "menos","que", "así","como", "Si", "tal",  "dado", "siempre",
+                                          "aunque", "pesar", "si", "por más que", "lo",
+                                          "así", "luego", "tan", "conque", "fin",
+                                          "Cuando", "mientras", "antes", "apenas", "cuanto" ))
+      # preposiciones
+      docs <- tm_map(docs, removeWords, c("a","durante","según","ante","en","sin","bajo","entre","so","cabe","hacia",
+                                          "sobre","con","hasta","tras","contra","mediante","versus","de","para","vía","desde","por"	)) 
+      docs <- tm_map(docs, removePunctuation) 
+      docs <- tm_map(docs, stripWhitespace) 
+      docs <- tm_map(docs, stemDocument)
+      dtm <- TermDocumentMatrix(docs) 
+      m <- as.matrix(dtm)
+      v <- sort(rowSums(m),decreasing=TRUE)
+      d <- data.frame(word = names(v),freq=v)
+      
+    }
+    
+  })
+  
+  
+  
+  fn_titulo = reactive({
+    
+    if (length(input$parrafo) == 0) {
+      
+    } else { 
+      link_1 <- import("https://raw.githubusercontent.com/VictorEnamorado/prueba_nube/main/buko2.xlsx")
+      req(input$parrafo) 
+      titulo <- link_1[as.numeric(input$parrafo),1]
+      
+    }
+    
+  })
+  
+  
+  output$titulo1 <- renderText({
+    paste0(fn_titulo())
+  })
+  
+  
+  
+  output$distPlot <- renderWordcloud2({ 
+    
+    wordcloud2(data=fn_parrafo(),size = 1,  color = 'random-light',backgroundColor = '#ffffff')
+    
+    
+  })
+  
+  
+  
+  #############################################################---- Tablas
+  
+ 
+  v <- reactiveValues(doPlot = FALSE) #<- variable reactiva
+  
+  observeEvent(input$go, { 
+    v$doPlot <- TRUE
+  })
+  
+  output$repetidos <- renderDT({
+    if (v$doPlot == FALSE) return()
+    
+    isolate({
+      
+      
+      
+      # output$frecuencia_plot <- renderPlot({
+      output$frecuencia_plot <- renderPlotly({
+        
+ 
+        fig <- plot_ly(
+          x = fn_parrafo()[1:10,]$word,
+          y = fn_parrafo()[1:10,]$freq,
+          name = "Palabras",
+          type = "bar"
+        )
+        
+        fig
+        
+        
+        
+        
+      })
+      
+      
+      fn_parrafo()
     })
+  })
+  
+  
+  
+  
+  
+  output$frecuencia <- renderDT({
     
-    
-    output$repetidos <- renderDT({
-        
-        tabla1 <- data.frame(findFreqTerms(dtm, lowfreq = 3))
-        names(tabla1)[1] <- "Palabras_repetidas"
-        tabla1
-        
-    })
-    
-    output$frecuencia <- renderDT({
-        
-        d
-        
-    })
-    
-    
-    
-    
-
+  })
+  
+  
+  
+  
+  #############################################################
+  
+  ########################
+  observe({
+    query <- parseQueryString(session$clientData$url_search)
+    if (!is.null(query[['parrafo']])) {
+      updateTextInput(session, "parrafo", value = query[['parrafo']])
+    } 
+    if (!is.null(query[['go']])) {
+      updateTextInput(session, "go", value = query[['go']])
+    } 
+  })
+  ########################
+  
+  
+  
 })
--- shiny_wordcloud/ui.R
+++ shiny_wordcloud/ui.R
@@ -1,82 +1,63 @@
- 
+
 
 library(shiny)
+library(dplyr)
 library("tm")
 library("SnowballC")
 library("wordcloud")
 library("RColorBrewer")
-
-
-##################################################################################################################
-
-text <- read.delim2("C:/Users/enamo/Desktop/Shiny-R/ds_wordcloud/buko.txt", encoding="UTF-8")
-
-# Load the data as a corpus
-docs <- Corpus(VectorSource(text))
-inspect(docs)
-
-toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
-docs <- tm_map(docs, toSpace, "/")
-docs <- tm_map(docs, toSpace, "@")
-docs <- tm_map(docs, toSpace, "\\|")
-
-# Convert the text to lower case
-docs <- tm_map(docs, content_transformer(tolower))
-# Remove numbers
-docs <- tm_map(docs, removeNumbers)
-# Remove english common stopwords
-docs <- tm_map(docs, removeWords, stopwords("english"))
-# Remove your own stop word
-# specify your stopwords as a character vector
-docs <- tm_map(docs, removeWords, c("blabla1", "blabla2")) 
-# Remove punctuations
-docs <- tm_map(docs, removePunctuation)
-# Eliminate extra white spaces
-docs <- tm_map(docs, stripWhitespace)
-
-dtm <- TermDocumentMatrix(docs)
-m <- as.matrix(dtm)
-v <- sort(rowSums(m),decreasing=TRUE)
-d <- data.frame(word = names(v),freq=v)
-
-
-
-##################################################################################################################
+library("rio")
+library(DT)
+library(wordcloud2)
 
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot", height = "500px" ),
-            
-            
-            
-            
-            fluidRow(
-                column(6, align="center", DTOutput("repetidos")),
-                column(6, align="center", DTOutput("frecuencia"), 
-                       
-                ), 
-            
-            
-             
-            
-        )
-        )
-    )
+  
+  # Application title
+  
+  fluidRow(
+    column(4),
+    column(2, align="center",
+           
+           # sliderInput("anio",
+           #             "",
+           #             min = 2002,
+           #             max = 2021,
+           #             sep = "",
+           #             value = 2002,
+           #             # width = "900px",
+           #             animate = animationOptions(interval = 1000,loop = TRUE))
+           # fileInput("csvs", label = "Subir serie de tiempo en formato .csv", multiple = TRUE),
+           titlePanel(h1("Wordcloud")),
+    ),
+    column(2, align="center",
+           
+           # img(height = 105, width = 350, src = "https://www.dataintelligence-group.com/Content/img/blanco-trans.png"),
+           img(height = 85, width = 340, src = "https://raw.githubusercontent.com/Sud-Austral/LOGOS-DATA/main/logo_dataIntelligence_normal.png"),
+    ), column(4)),
+  
+  # titlePanel("Mineria de datos"),
+  
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      
+      div(style="display: none;",textInput(inputId = "parrafo",label = "parrafo")),
+      fluidRow(
+        column(6),
+        column(6, align="center", wordcloud2Output("distPlot", height = "500px" )),
+      ),
+      fluidRow(
+        column(4, align="center", DTOutput("repetidos")),
+        column(4, align="center", DTOutput("frecuencia")), 
+        column(4, align="center", plotOutput("frecuencia_plot")), 
+      )
+      
+      
+    ),
+    mainPanel()
+  )
 ))
